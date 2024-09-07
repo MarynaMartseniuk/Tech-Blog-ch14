4e9211969e0fe4977872fbd0f79cf5369d591c83
MarcM <marina.ved80gmail.com>
2024-09-06 20:26:03 -0600

    add models

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const bcrypt = require('bcrypt');
+const User = require('../../models/User');
+
+// Added comments describing the functionality of this `login` route
+router.post('/login', async (req, res) => {  
+  try {
+    // find a user in db by username
+    const userData = await User.findOne({ where: { username: req.body.username } });
+    if (!userData) {
+        res.status(404).json({ message: 'Login failed. Please try again!' });
+        return;
+    };
+    // verify password
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      userData.password
+    );
+    if (!validPassword) {
+      res.status(400).json({ message: 'Login failed. Please try again!' });
+      return;
+    }
+    // if user was found by provided username and password was verified then go to personal-page
+    res.status(200).json({ message: 'You are now logged in!' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+  },
+  {
+    hooks: {
+      // Use the beforeCreate hook to work with data before a new instance is created
+      beforeCreate: async (newUserData) => {
+        // In this case, we are taking the user's email address, and making all letters lower case before adding it to the database.
+        newUserData.username = await newUserData.username.toLowerCase();
+        return newUserData;
+      },
+      // Here, we use the beforeUpdate hook to make all of the characters lower case in an updated email address, before updating the database.
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.username = await updatedUserData.username.toLowerCase();
+        return updatedUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
