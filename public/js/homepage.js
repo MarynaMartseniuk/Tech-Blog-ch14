7fd73620ad27a2a428c198d7fdf444912d07a1b7
MarcM <marina.ved80gmail.com>
2024-09-08 17:34:26 -0600

    add logic to view

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,1 +1,23 @@
 //TODO display all posts that were done by all users
+
+const displayAllPosts = async () => {
+  
+    // const name = document.querySelector('#project-name').value.trim();
+    // const needed_funding = document.querySelector('#project-funding').value.trim();
+    // const description = document.querySelector('#project-desc').value.trim();
+  
+    
+    const response = await fetch(`/`, {
+        method: 'GET',
+        //headers: {'Content-Type': 'application/json',},
+    });
+
+    if (response.ok) {
+        const postsData = await response.json();
+        console.log(postsData);
+    } else {
+    alert('Failed to get posts');
+    }
+    
+  };
+  
