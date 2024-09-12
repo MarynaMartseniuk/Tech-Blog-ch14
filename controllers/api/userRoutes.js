1bfbfe255b52fa5d3c3981ffd22ea5fd4e30be1c
MarcM <marina.ved80gmail.com>
2024-09-12 14:07:56 -0600

    login username

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -28,6 +28,7 @@ router.post('/login', async (req, res) => {
 
     req.session.save(() => {
       req.session.user_id = loggedInUser.id;
+      req.session.user_username = loggedInUser.username;
       req.session.logged_in = true;
 
       res.status(200).json({ user: userData, message: 'You are now logged in!' });
